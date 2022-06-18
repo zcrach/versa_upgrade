73e2e231221fb2f9b4575baa7be8cdb993316766
Michael Behrns <michael_behrns@yahoo.no>
2022-06-18 19:06:01 +0200

    cronjob Updates hourly

--- pxssh.py
+++ pxssh.py
@@ -1,6 +1,8 @@
 import pexpect
 import getpass
+import json
 import sys
+import re
 
 prompt = "\$"
 
@@ -9,16 +11,25 @@ username = "admin"
 password = "versa123"
 
 
+def versa_find_attribute(command_output, matching_string):
+    variable_get = re.findall(r"[\n\r].*" + matching_string + "\s*([^\n\r\t]*)", command_output)
+    return variable_get
+
+
 ch = pexpect.spawn(f'ssh {username}@{hostname}')
 #ch.logfile = sys.stdout.buffer
 ch.expect('assword:')
 ch.sendline(password)
 ch.expect(prompt)
-ch.sendline('vsh details | grep -o "Serial number.*"') 
+ch.sendline('vsh details') 
 ch.expect(prompt)
-output = ch.before.decode('utf-8').splitlines()
+output = ch.before.decode()
 ch.sendline('exit')
-#print(output)
-line_number = 0
-for line in output:
-    print(line)
+serial_number = versa_find_attribute(output, "Serial number")
+release = versa_find_attribute(output, "Release")
+package_name = versa_find_attribute(output, "Package name")
+print(serial_number)
+print(release)
+print(package_name)
+
+
